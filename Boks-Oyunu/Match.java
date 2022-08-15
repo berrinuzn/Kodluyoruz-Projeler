22c2ad75fe02b6490f04b4ecfdedefd3d236dc34
Berrin Uzun <109957997+berrinuzn@users.noreply.github.com>
2022-08-15 17:36:21 +0300

    Add files via upload

---
+++ Boks-Oyunu/Match.java
@@ -0,0 +1,71 @@
+
+public class Match {
+
+	Fighter f1 ;
+	Fighter f2 ;
+	int minWeight ;
+	int maxWeight ;
+	
+	public Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+		this.f1 = f1;
+		this.f2 = f2;
+		this.minWeight = minWeight;
+		this.maxWeight = maxWeight;
+	}
+	
+	
+	void run() {
+		
+		if(isCheck()) {
+			
+			while(this.f1.health > 0 && this.f2.health > 0) {
+				System.out.println("=====YENÝ ROUND=====");
+				double random = Math.random()*100 ;
+				if(random >= 0 && random < 50) {
+				this.f2.health = this.f1.hit(this.f2) ;
+				if(isWin()) {
+					break ;
+				}
+				}
+				else {
+				this.f1.health = this.f2.hit(this.f1) ;
+				if(isWin()) {
+					break ;
+				}
+				}
+				System.out.println(this.f1.name + " Saðlýk : " + this.f1.health);
+				System.out.println(this.f2.name + " Saðlýk : " + this.f2.health);
+				
+			}
+			
+		}
+		else {
+			
+			System.out.println("Sporcularýn sikletleri uymuyor.");
+			
+		}
+	}
+	
+	boolean isCheck() {
+		return (this.f1.weight >= minWeight && this.f1.weight <= maxWeight) && (this.f2.weight >= minWeight && this.f2.weight <= maxWeight) ;
+	}
+	
+	boolean isWin() {
+		
+		if(this.f1.health == 0) {
+			System.out.println(this.f2.name + " kazandý !");
+			return true ;
+		}
+		if(this.f2.health == 0) {
+			System.out.println(this.f1.name + " kazandý !");
+			return true ;
+		}
+		
+		
+		
+		return false ;
+	}
+	
+	
+	
+}
